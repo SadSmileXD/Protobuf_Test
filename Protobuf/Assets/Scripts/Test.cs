643ea36f7279a3964e06a36a171b09f2258b7b33
SadSmileXD <phycoeco@naver.com>
2026-09-21 13:01:44 +0900

    push

---
+++ Protobuf/Assets/Scripts/Test.cs
@@ -0,0 +1,32 @@
+﻿using PrptpPlayer;
+using System.IO;
+using UnityEngine;
+using Google.Protobuf;
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var data = new PlayerData
+        {
+            Level = 10,
+            UserId = 12345,
+            UserName = "PlayerOne",
+            Damage = 250
+        };
+
+        byte[] bytes;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            data.WriteTo(stream);
+            bytes = stream.ToArray();
+        }
+        Debug.Log($"Serialized Data: {bytes.Length}");
+        var deserializedData = PlayerData.Parser.ParseFrom(bytes);
+        Debug.Log($"Deserialized Data: UserId={deserializedData.UserId}, UserName={deserializedData.UserName}, Level={deserializedData.Level}, Damage={deserializedData.Damage}");
+    }
+}
+
+    
+
+
